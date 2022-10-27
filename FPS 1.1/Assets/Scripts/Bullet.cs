282d261d730ebf6c8b5ea67fb06575619fc242e0
Gekire156 <114258803+Gekire156@users.noreply.github.com>
2022-10-27 13:36:10 -0500

    1.1 FPS
    Added bullet sounds, dont push enemies anymore, and bullet shoots out of barrel

---
+++ FPS 1.1/Assets/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.IgnoreCollision(player.GetComponent<Collider>(), this.GetComponent<Collider>());
+        Physics.IgnoreCollision(this.GetComponent<Collider>(), this.GetComponent<Collider>());
+
+    }
+    private void OnTriggerEnter(Collider collision){
+        Destroy(this.gameObject);
+        if (collision.gameObject.TryGetComponent<Enemy>(out Enemy enemycomponent))
+        {
+            enemycomponent.TakeDamage(10);
+            Destroy(this.gameObject);
+        }
+    }
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+        if (collision.gameObject.TryGetComponent<Enemy>(out Enemy enemycomponent))
+        {
+            enemycomponent.TakeDamage(10);
+            Destroy(this.gameObject);
+        }
+    }*/
+}
